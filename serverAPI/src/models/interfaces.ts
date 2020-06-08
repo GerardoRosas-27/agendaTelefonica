3fe90705388b6d4c5a909eb4ce0e25fc40bfeb75
GerardoRosas-27 <brustergabriel24@gmail.com>
2020-06-08 16:44:22 -0500

    primer commit API rest contactos y usuarios

---
+++ serverAPI/src/models/interfaces.ts
@@ -0,0 +1,16 @@
+export interface Usuarios {
+    id?: number;
+    nombre: string;
+    correo: string;
+    contra: string;
+}
+
+export interface Contactos {
+    id?: number;
+    nombre: string;
+    telefono: string;
+    correo: string;
+    fecha: string;
+    usuario: number;
+
+}
