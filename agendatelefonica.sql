b96845da90bbb59796a3c5e36d7686fdc77f3731
GerardoRosas-27 <brustergabriel24@gmail.com>
2020-06-09 14:36:12 -0500

    config bd

--- agendatelefonica.sql
+++ agendatelefonica.sql
@@ -1,54 +1,116 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5977
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-06-2020 a las 21:22:23
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table agendatelefonica.contactos
-CREATE TABLE IF NOT EXISTS `contactos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Base de datos: `agendatelefonica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactos`
+--
+
+CREATE TABLE `contactos` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   `correo` varchar(100) DEFAULT NULL,
   `fecha` datetime DEFAULT NULL,
-  `usuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_contactos_usuarios` (`usuario`),
-  CONSTRAINT `FK_contactos_usuarios` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+  `usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table agendatelefonica.contactos: ~0 rows (approximately)
-/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `contactos`
+--
+
 INSERT INTO `contactos` (`id`, `nombre`, `telefono`, `correo`, `fecha`, `usuario`) VALUES
-	(12, 'gerardo', '2424234', 'bruster@outlook.es', '2025-07-24 05:00:00', 1),
-	(14, 'gerarado', '2423432432', 'bruster', '2020-06-24 05:00:00', 3),
-	(15, 'gerardo', '24324234', 'bruster@outlook.es', '2020-06-18 05:00:00', 1);
-/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+(12, 'gerardo', '2424234', 'bruster@outlook.es', '2025-07-24 05:00:00', 1),
+(14, 'gerarado', '2423432432', 'bruster', '2020-06-24 05:00:00', 3),
+(15, 'gerardo', '24324234', 'bruster@outlook.es', '2020-06-18 05:00:00', 1);
 
--- Dumping structure for table agendatelefonica.usuarios
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) DEFAULT NULL,
   `correo` varchar(100) DEFAULT NULL,
-  `contra` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  `contra` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table agendatelefonica.usuarios: ~2 rows (approximately)
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
 INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `contra`) VALUES
-	(1, 'Gerardo Rosas 2', 'bruster@outlook.es', '$2b$10$np9kCS.c0KOvea6cjQ5NcOBiwiqa7KB81Ow4UgdwhoVETERXcc.v6'),
-	(2, 'bruster', 'bruster_24@outlook.es', '$2b$10$E2mkSGRyumQrp5Hdk6geweV80qnQ0sWbxus/DBZqnbYNGaPwZ3qSK'),
-	(3, 'bruster', 'bruster_25@outlook.es', '$2b$10$uLfj7KAdfV9eVB1yxnimUOS1b.4qu06P4ZkrzEU/yamXUBEleBokm');
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+(1, 'Gerardo Rosas 2', 'bruster@outlook.es', '$2b$10$np9kCS.c0KOvea6cjQ5NcOBiwiqa7KB81Ow4UgdwhoVETERXcc.v6'),
+(2, 'bruster', 'bruster_24@outlook.es', '$2b$10$E2mkSGRyumQrp5Hdk6geweV80qnQ0sWbxus/DBZqnbYNGaPwZ3qSK'),
+(3, 'bruster', 'bruster_25@outlook.es', '$2b$10$uLfj7KAdfV9eVB1yxnimUOS1b.4qu06P4ZkrzEU/yamXUBEleBokm');
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_contactos_usuarios` (`usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `contactos`
+--
+ALTER TABLE `contactos`
+  ADD CONSTRAINT `FK_contactos_usuarios` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
